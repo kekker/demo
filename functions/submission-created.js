a8ea2dabae6f3d9aa79a5efb1e0c8a4cdba67fc0
Puzanira <puzanova.ira2011@yandex.ru>
2020-03-27 16:29:01 +0300

    Fixing form

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -2,7 +2,7 @@ const { API_MESSAGING_AUTH_BASIC_KEY } = process.env;
 
 import BackendFetcher from "../managers/BackendFetcher";
 
-exports.handler  = function(event, context, callback) {
+exports.handler  = async (event, context, callback) => {
     const payload = JSON.parse(event.body).payload;
     const headers = {
             Accept: "application/json",
@@ -10,25 +10,17 @@ exports.handler  = function(event, context, callback) {
             Authorization: "Basic " + API_MESSAGING_AUTH_BASIC_KEY,
     };
     console.log('Submission-created lambda triggered');
-    BackendFetcher.post('api/messages/', {payload, headers})
-        .then((body) => {
-            const { data } = body;
-            const { errors } = data;
-            if (errors) {
-                console.log('failed');
-                return callback(null, {
-                    statusCode: error.status,
-                    body: JSON.stringify({
-                        message: error.message,
-                        error: error,
-                    })
-                })
-            }
-            console.log('saved');
-            return callback (null, {
-                statusCode: 200,
-                body: 'Submission saved successfully'
-            })
+    try {
+        await BackendFetcher.post('api/messages/', {payload, headers});
+
+        return {
+            statusCode: 200,
+            body: 'Request send'
         }
-    )
+    } catch (error) {
+        return {
+            statusCode: e.code,
+            body: e.message
+        }
+    }
 };
