9ce17c5a246d1af5908f855dc1edccafb1d440bf
Puzanira <puzanova.ira2011@yandex.ru>
2020-03-27 17:00:48 +0300

    Fixing form

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,6 +1,36 @@
 const { API_MESSAGING_AUTH_BASIC_KEY } = process.env;
+const baseBackendUrl = 'http://demoplatform1.kekker.com';
+const messageRoute = '/api/messages';
 
-import BackendFetcher from "../managers/BackendFetcher";
+class BackendFetcher {
+    static getBackendUrl(url) {
+        return `${baseBackendUrl}${url}`;
+    }
+
+    static setup() {
+        // Get a CSRF Token from a db with Cookies
+    }
+
+    static post(url, params = {}) {
+        const {
+            payload: data,
+            headers,
+            serialize = true,
+        } = params;
+
+        // Get CSRFToken
+        return fetch(this.getBackendUrl(url), {
+            method: 'POST',
+            // credentials: 'include',
+            // mode: 'cors',
+            headers: {
+                ...headers,
+                // 'X-CSRFToken': CSRFToken,
+            },
+            body: serialize ? JSON.stringify({ data }) : data,
+        }).then((response) => response.json())
+    }
+}
 
 exports.handler  = async (event, context, callback) => {
     const payload = JSON.parse(event.body).payload;
