38567555a7c67df50752c11bbb7d0951f7e995ab
Puzanira <32363188+Puzanira@users.noreply.github.com>
2020-04-13 13:09:24 +0300

    Deleted Zoom package

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,5 @@
 // import './src/styles/blog.css'
 import './src/styles/global.css';
 import './src/styles/gatsby-code.css';
-import 'react-medium-image-zoom/dist/styles.css';
 // import './src/styles/posts.css'
 require("prismjs/themes/prism-solarizedlight.css");
