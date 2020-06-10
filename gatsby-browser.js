131dcbfbc6a51887ccd616475e3ba71d6187784e
Puzanira <puzanova.ira2011@yandex.ru>
2020-06-10 17:22:59 +0300

    Prismjs styles added

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,18 @@
 import './src/styles/global.css';
 import './src/styles/gatsby-code.css';
 
+import React from 'react';
+import { AppStateProvider } from './src/appState/appState';
+import valuesStateReducer, { initialState } from './src/appState/appReducer';
+
+
 require('prismjs/themes/prism-solarizedlight.css');
+
+export const wrapRootElement = ({ element }) => (
+  <AppStateProvider
+    reducer={valuesStateReducer}
+    initialState={initialState}
+  >
+    {element}
+  </AppStateProvider>
+);
